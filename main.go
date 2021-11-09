84268ba99e334d56b7f3e06ffbaf2df2253f4921
pgaudenzi <pia.gaudenzi@mercadolibre.com>
2021-11-09 10:19:47 -0300

    structure

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(primeNumbers(23))
+}
+
+/*
+	Return all prime numbers less than or equal to N
+
+	A prime number is a natural number greater than 1, which is only divisible by 1 and itself.
+	First few prime numbers are : 2 3 5 7 11 13 17 19 23 …..
+*/
+func primeNumbers(n int) []int {
+	var primes []int
+
+
+	return primes
+}
